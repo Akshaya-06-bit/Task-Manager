b21ae027e9ef55b961ba429423964a8d1336ebba
Akshaya Bollampally <akshayabollampally@gmail.com>
2026-04-03 21:13:14 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://task-manager-7ldh.onrender.com/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: API_URL,
 });
 
-// POST /api/auth/register
 export const registerUser = (userData) => api.post("/auth/register", userData);
-
-// POST /api/auth/login
 export const loginUser = (userData) => api.post("/auth/login", userData);
